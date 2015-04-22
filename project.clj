742b6d3f0908d9c2f214b8828af9fbde1c7eb3c2
Martin Cooper <martin.cooper@me.com>
2015-04-22 19:47:52 +0100

    Adding initial source files.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clj-try "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :description "clj-try: Exception macros"
+  :url "https://github.com/martincooper/clj-try"
+  :license {:name "Eclipse Public LicenseApache License Version 2.0"
+            :url "http://www.apache.org/licenses/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :min-lein-version "2.0.0"
+  :uberjar-name "clj-try-standalone.jar"
+  :profiles {:production {:env {:production true}}})
--- src/clj_try/core.clj
+++ src/clj_try/core.clj
@@ -1,6 +1,31 @@
 (ns clj-try.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn bind-error [f [val err]]
+  (if (nil? err)
+    (f val)
+    [nil err]))
+
+; Try function.
+(defn try> [fn]
+  ())
+
+; Try thread last macro.
+(defmacro try->> [val & fns]
+  (let [fns (for [f fns] `(bind-error ~f))]
+    `(->> [~val nil]
+          ~@fns)))
+
+; Try thread first macro.
+(defmacro try-> [val & fns]
+  (let [fns (for [f fns] `(bind-error ~f))]
+    `(-> [~val nil]
+          ~@fns)))
+
+; Try thread as macro.
+(defmacro try-as->> [val & fns]
+  (let [fns (for [f fns] `(bind-error ~f))]
+    `(->> [~val nil]
+          ~@fns)))
+
+(defn test1 []
+  (try> "aa"))
