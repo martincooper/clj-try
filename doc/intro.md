f82279703dba24001f72b38ccea634ce7287e9fd
Martin Cooper <martin.cooper@me.com>
2015-04-22 19:14:19 +0100

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-try
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_try/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-try.core-test
+  (:require [clojure.test :refer :all]
+            [clj-try.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
