8cbc07b3cd883e940ec8683a3eba951ad841f982
Martin Cooper <martin.cooper@me.com>
2015-04-27 19:00:00 +0100

    Additional unit tests.

--- test/clj_try/core_test.clj
+++ test/clj_try/core_test.clj
@@ -8,11 +8,28 @@
   (is (= (.getMessage ex) msg)))
 
 (deftest try-macro-pass
-  (testing "Try macro. Pass case."
+  (testing "Testing try> macro. Pass case."
     (let [result (try> (str "12345"))]
       (is (= result { :value "12345" })))))
 
-(deftest try-macro-failure
-  (testing "Try macro. Exception case."
+(deftest try-macro-exception
+  (testing "Testing try> macro. Exception case."
     (let [result (try> (throw (NullPointerException. "Test Error")))]
+      (check-exception result "Test Error"))))
+
+(deftest bind-error-last-pass
+  (testing "Testing bind-error->>. Pass case."
+    (let [result (bind-error->> (str "aaa") { :value "bbb"} )]
+      (is (= result { :value "aaabbb" })))))
+
+(deftest bind-error-last-error
+  (testing "Testing bind-error->>. Error case."
+    (let [result (bind-error->> (str "aaa") { :error "Test Error"} )]
+      (is (= result { :error "Test Error" })))))
+
+(deftest bind-error-last-exception
+  (testing "Testing bind-error->>. Exception case."
+    (let [result (bind-error->>
+                  (str "aaa")
+                  { :error (NullPointerException. "Test Error")} )]
       (check-exception result "Test Error"))))
