378820484832d6ca3c838bf1ae70a77472218aae
Martin Cooper <martin.cooper@me.com>
2015-04-27 17:44:42 +0100

    Updating macros.

--- src/clj_try/core.clj
+++ src/clj_try/core.clj
@@ -1,7 +1,7 @@
 (ns clj-try.core)
 
 (defmacro try>
-  "Takes an expression and wraps it in a try / catch.
+  "Takes an expression and wraps it in a try / catch
   returning the value or exception in a map."
   [fn]
   `(try
@@ -9,52 +9,38 @@
      (catch Exception ex#
        { :error ex# })))
 
-; If value, apply function with value. If error, return error.
-(defn bind-error
-  "Bind method. Determines if to run the expression or return the
+(defmacro bind-error->>
+  "Bind error last. Determines if to run the expression or return the
   previous result if an error occurred."
-  [f {val :value err :error :as all}]
-  (if (nil? err)
-    (try> (f val))
-    all))
+  [fn value]
+  `(let [{val# :value err# :error :as all#} ~value]
+     (if (nil? err#)
+       (try> (->> val# ~fn))
+       all#)))
 
-
-(defmacro my->>
-  "Threads the expr through the forms. Inserts x as the
-  last item in the first form, making a list of it if it is not a
-  list already. If there are more forms, inserts the first form as the
-  last item in second form, etc."
-  {:added "1.1"}
-  [x & forms]
-  (loop [x x, forms forms]
-    (if forms
-      (let [form (first forms)
-            threaded (if (seq? form)
-              (with-meta `(~(first form) ~@(next form)  ~x) (meta form))
-              (list form x))]
-        (recur threaded (next forms)))
-      x)))
-
-
-(defmacro err->> [val & fns]
-  (let [fcns (for [f fns] `(bind-error ~f))]
-    `(->> ~val
+(defmacro try->>
+  "Try last. Similar to the thread last macro, but wrapping each expression
+  in a try / catch and passing the result to the following expression. If an error
+  occurs, the chain will be short circuited returning the first error occurred."
+  [val & fns]
+  (let [fcns (for [f fns] `(bind-error->> ~f))]
+    `(->> (try> ~val)
           ~@fcns)))
 
-(defmacro try->> [val & fns]
-  (let [fns (for [f fns] `~f)]
-    `(->> ~val
-          ~@fns)))
+(defmacro bind-error->
+  "Bind error last. Determines if to run the expression or return the
+  previous result if an error occurred."
+  [fn value]
+  `(let [{val# :value err# :error :as all#} ~value]
+     (if (nil? err#)
+       (try> (-> val# ~fn))
+       all#)))
 
-
-(defn test1 []
-  (try->>
-   (str "aa")
-   (str "bb")
-   (str "cc")))
-
-
-(defn test2 []
-  (err->>
-   (str "bb")
-   (str "cc")))
+(defmacro try->
+  "Try last. Similar to the thread last macro, but wrapping each expression
+  in a try / catch and passing the result to the following expression. If an error
+  occurs, the chain will be short circuited returning the first error occurred."
+  [val & fns]
+  (let [fcns (for [f fns] `(bind-error-> ~f))]
+    `(-> (try> ~val)
+          ~@fcns)))
