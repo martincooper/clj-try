3ebbe14d2f72abe86d71ab5e9965a0e0b4ec95c6
Martin Cooper <martin.cooper@me.com>
2015-04-27 18:48:37 +0100

    Adding initial unit tests.

--- test/clj_try/core_test.clj
+++ test/clj_try/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [clj-try.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn check-exception
+  [{ ex :error } msg]
+  (is (instance? NullPointerException ex))
+  (is (= (.getMessage ex) msg)))
+
+(deftest try-macro-pass
+  (testing "Try macro. Pass case."
+    (let [result (try> (str "12345"))]
+      (is (= result { :value "12345" })))))
+
+(deftest try-macro-failure
+  (testing "Try macro. Exception case."
+    (let [result (try> (throw (NullPointerException. "Test Error")))]
+      (check-exception result "Test Error"))))
