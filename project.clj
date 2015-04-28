f6ec2f60ad8a5185327638555debf2db805e48b0
Martin Cooper <martin.cooper@me.com>
2015-04-28 19:26:42 +0100

    Updating project.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-try "0.1.0-SNAPSHOT"
-  :description "clj-try: Exception macros"
+  :description "clj-try: Try / Error macros"
   :url "https://github.com/martincooper/clj-try"
-  :license {:name "Eclipse Public LicenseApache License Version 2.0"
+  :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :min-lein-version "2.0.0"
